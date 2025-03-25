e304e573efe651b509fffdcc4de8f58c4c52944e
Carlos Huaccho <Carlos@gmail.com>
2025-03-25 11:51:37 -0400

    fixing utility function

--- langchain_scripts/utility.py
+++ langchain_scripts/utility.py
@@ -2,4 +2,15 @@ from pathlib import Path
 
 
 def find_root_directory(file: str) -> Path:
-    return Path(file).parent.parent
+    """Find the root directory of the project.
+
+    Args:
+        file (str): The full path to the file.
+
+    Returns:
+        Path: The root directory of the project.
+    """
+    current_dir: Path = Path(file).parent
+    while "poetry.lock" not in [i.name for i in current_dir.iterdir()]:
+        current_dir = current_dir.parent
+    return current_dir
